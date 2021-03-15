abe8a13dd4321cd5f786d8a6eeb3c6dc6c5006ea
Ines Radic <inesradic12314@gmail.com>
2021-03-15 00:16:48 -0700

    add app and mongoose connection

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3000
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/tracker", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+// routes
+
+
+// app listening
+app.listen(PORT, () => {
+    console.log(`App is listening on port: ${PORT}`);
+});
