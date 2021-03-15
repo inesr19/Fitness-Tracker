d4e1f8b309234fe1d3f4f116a0978a126a05d894
Ines Radic <inesradic12314@gmail.com>
2021-03-15 13:56:10 -0700

    set up api routes and create rest file

--- routes/api-route.js
+++ routes/api-route.js
@@ -1,4 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+// Get all workouts
+router.get('/', (req, res) => {
+    res.send('hello world')
+})
+// Get one workout
+router.get('/:id', (req, res) => {
+    req.params.id
+})
+// Create one workout
+router.post('/', (req, res) => {
+    
+})
+// Update workout
+router.patch('/:id', (req, res) => {
+
+})
+// Delete workout
+router.delete('/:id', (req, res) => {
+    
+})
+
 module.exports = router
