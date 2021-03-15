7b67fd909b001f267e373c0f05e2e2f686eb4dc3
Ines Radic <inesradic12314@gmail.com>
2021-03-15 14:45:17 -0700

    GET stats html route

--- routes/html-route.js
+++ routes/html-route.js
@@ -6,5 +6,8 @@ module.exports = (app) => {
     });
     app.get('/exercise', (req, res) => {
         res.sendFile(path.join(_dirname, '../public/exercise.html'));
+    });
+    app.get('/stats', (req, res) => {
+        res.sendFile(path.join(_dirname, '../public/stats.html'));
     })
 }
