b05f48eae4e7b35c94a671ca5e2140de258b7664
Ines Radic <inesradic12314@gmail.com>
2021-03-14 23:55:42 -0700

    add schema types and export

--- models/tracker.js
+++ models/tracker.js
@@ -1,2 +1,41 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const trackerSchema = new Schema ({
+    day: {
+        type: Date,
+        default: () => new Date()
+    },
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: 'Enter the type of exercise.'
+        },
+    name: {
+        type: String,
+        trim: true,
+        required: 'Enter the name of the exercise.'
+    },
+    duration: {
+        type: Number,
+        required: 'Enter the duration of the exercise (mins).'
+    },
+    weight: {
+        type: Number
+    },
+    reps: {
+        type: Number
+    },
+    sets: {
+        type: Number
+    },
+    distance: {
+        type: Number
+    }
+    }]
+});
+
+const Tracker = mongoose.model("Tracker", trackerSchema);
+
+module.exports = Tracker;
